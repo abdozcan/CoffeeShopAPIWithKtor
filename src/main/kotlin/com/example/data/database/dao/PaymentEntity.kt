a9725e2f95b26ff6d24b01a251b97d3accdcaa30
abdozcan <abdozcann@gmail.com>
2024-10-12 21:33:52 +0300

    edited PaymentEntity

--- src/main/kotlin/com/example/data/database/dao/PaymentEntity.kt
+++ src/main/kotlin/com/example/data/database/dao/PaymentEntity.kt
@@ -9,7 +9,7 @@ import org.jetbrains.exposed.dao.id.EntityID
 class PaymentEntity(id: EntityID<Int>) : IntEntity(id) {
     companion object : IntEntityClass<PaymentEntity>(PaymentTable)
 
-    val orderId by OrderEntity referencedOn OrderTable.id
+    val orderId by PaymentTable.orderId
     val paymentDate by PaymentTable.paymentDate
     val amount by PaymentTable.amount
     val method by PaymentTable.method

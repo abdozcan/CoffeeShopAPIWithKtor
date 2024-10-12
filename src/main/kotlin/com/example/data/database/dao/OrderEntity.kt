b9f1f38e55c5086c287ef489b386ec2f74c9dd83
abdozcan <abdozcann@gmail.com>
2024-10-12 18:05:02 +0300

    edited OrderEntity and added orderItems variable within it

--- src/main/kotlin/com/example/data/database/dao/OrderEntity.kt
+++ src/main/kotlin/com/example/data/database/dao/OrderEntity.kt
@@ -1,7 +1,7 @@
 package com.example.data.database.dao
 
+import com.example.data.database.table.OrderItemTable
 import com.example.data.database.table.OrderTable
-import com.example.data.database.table.UserTable
 import org.jetbrains.exposed.dao.IntEntity
 import org.jetbrains.exposed.dao.IntEntityClass
 import org.jetbrains.exposed.dao.id.EntityID
@@ -9,8 +9,13 @@ import org.jetbrains.exposed.dao.id.EntityID
 class OrderEntity(id: EntityID<Int>) : IntEntity(id) {
     companion object : IntEntityClass<OrderEntity>(OrderTable)
 
-    val userId by UserEntity referencedOn UserTable.id
+    val userId by OrderTable.userId
     val orderDate by OrderTable.orderDate
     val amount by OrderTable.amount
     val status by OrderTable.status
+
+    private val orderItems by OrderItemEntity referrersOn OrderItemTable.orderId
+   /* val products = orderItems.mapNotNull {
+        ProductEntity.findById(it.productId)
+    }*/
 }

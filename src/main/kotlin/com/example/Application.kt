ed1408bf0f79efef40442d0b72923dc1435b1e7d
abdozcan <abdozcann@gmail.com>
2024-10-13 00:35:04 +0300

    edited Application.kt

--- src/main/kotlin/com/example/Application.kt
+++ src/main/kotlin/com/example/Application.kt
@@ -1,15 +1,53 @@
 package com.example
 
-import com.example.plugins.*
+import com.example.data.database.table.*
+import com.example.data.repository.DefaultProductRepository
+import com.example.data.repository.DefaultUserRepository
+import com.example.data.utils.dbQuery
+import com.example.domain.repository.ProductRepository
+import com.example.domain.repository.UserRepository
+import com.example.plugins.configureSecurity
+import com.example.routes.productRoutes
+import com.example.routes.userRoutes
+import io.ktor.serialization.kotlinx.json.*
 import io.ktor.server.application.*
+import io.ktor.server.netty.*
+import io.ktor.server.plugins.contentnegotiation.*
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.launch
+import org.jetbrains.exposed.sql.Database
+import org.jetbrains.exposed.sql.SchemaUtils
+import org.jetbrains.exposed.sql.transactions.transaction
 
 fun main(args: Array<String>) {
-    io.ktor.server.netty.EngineMain.main(args)
+    EngineMain.main(args)
 }
 
 fun Application.module() {
-    configureSerialization()
-    configureDatabases()
-    configureSecurity()
+    install(ContentNegotiation) { json() }
+    configureDatabase()
     configureRouting()
+    configureSecurity()
+}
+
+private fun Application.configureRouting() {
+    productRoutes(DefaultProductRepository())
+    userRoutes(DefaultUserRepository())
+}
+
+private fun Application.configureDatabase() {
+    launch {
+        transaction(
+            Database.connect(
+                "jdbc:sqlite:./data/coffee_shop_api.db",
+                "org.sqlite.JDBC"
+            )
+        ) {
+            SchemaUtils.create(
+                AddressTable, CategoryTable, FavoriteTable,
+                OrderItemTable, OrderTable, PaymentTable,
+                PromotionTable, ProductTable, ReviewTable, UserTable
+            )
+        }
+    }
 }

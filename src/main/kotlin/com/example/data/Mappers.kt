2b7997491047b8fa0447b120d0a12a708d25873c
abdozcan <abdozcann@gmail.com>
2024-10-12 21:34:43 +0300

    edited mappers

--- src/main/kotlin/com/example/data/Mappers.kt
+++ src/main/kotlin/com/example/data/Mappers.kt
@@ -1,70 +1,76 @@
 package com.example.data
 
-import com.example.data.database.table.*
-import com.example.data.model.*
-import org.jetbrains.exposed.sql.ResultRow
+import com.example.data.database.dao.*
+import com.example.domain.model.*
 
-fun ResultRow.toCategory() = Category(
-    id = this[CategoryTable.id].value,
-    name = this[CategoryTable.name],
-    description = this[CategoryTable.description]
+fun CategoryEntity.toCategory() = Category(
+    id = id.value,
+    name = name,
+    description = description
 )
 
-fun ResultRow.toUser() = User(
-    id = this[UserTable.id].value,
-    name = this[UserTable.name],
-    password = this[UserTable.password],
-    email = this[UserTable.email],
-    phone = this[UserTable.phone],
-    address = this[UserTable.address],
-    createdAt = this[UserTable.createdAt]
+fun UserEntity.toUser() = User(
+    id = id.value,
+    name = name,
+    password = password,
+    email = email,
+    phone = phone,
+    address = address,
+    createdAt = createdAt
 )
 
-fun ResultRow.toProduct() = Product(
-    id = this[ProductTable.id].value,
-    name = this[ProductTable.name],
-    description = this[ProductTable.description],
-    price = this[ProductTable.price],
-    region = this[ProductTable.region],
-    weight = this[ProductTable.weight],
-    flavorProfile = this[ProductTable.flavorProfile],
-    grindOption = this[ProductTable.grindOption],
-    roastLevel = this[ProductTable.roastLevel],
-    imageUrl = this[ProductTable.imageUrl],
-    category = this[ProductTable.category],
-    stockQuantity = this[ProductTable.stockQuantity],
-    popularityRating = this[ProductTable.popularityRating],
-    discountPrice = this[ProductTable.discountPrice],
-    discountPercentage = this[ProductTable.discountPercentage],
-    bestseller = this[ProductTable.bestseller]
+fun ProductEntity.toProduct() = Product(
+    id = id.value,
+    name = name,
+    description = description,
+    price = price,
+    region = region,
+    weight = weight,
+    flavorProfile = flavorProfile,
+    grindOption = grindOption,
+    roastLevel = roastLevel,
+    imageUrl = imageUrl,
+    category = category,
+    stockQuantity = stockQuantity,
+    popularityRating = popularityRating,
+    discountPrice = discountPrice,
+    discountPercentage = discountPercentage,
+    bestseller = bestseller
 )
 
-fun ResultRow.toOrder() = Order(
-    id = this[OrderTable.id].value,
-    userId = this[OrderTable.userId].value,
-    orderDate = this[OrderTable.orderDate],
-    amount = this[OrderTable.amount],
-    status = this[OrderTable.status]
+fun OrderEntity.toOrder() = Order(
+    id = id.value,
+    userId = userId.value,
+    orderDate = orderDate,
+    amount = amount,
+    status = status
 )
 
-fun ResultRow.toOrderItem() = OrderItem(
-    id = this[OrderItemTable.id].value,
-    orderId = this[OrderItemTable.orderId].value,
-    productId = this[OrderItemTable.productId].value,
-    quantity = this[OrderItemTable.quantity],
-    price = this[OrderItemTable.price]
+fun OrderItemEntity.toOrderItem() = OrderItem(
+    id = id.value,
+    orderId = orderId.value,
+    productId = productId.value,
+    quantity = quantity,
+    price = price
 )
 
-fun ResultRow.toPayment() = Payment(
-    id = this[PaymentTable.id].value,
-    orderId = this[PaymentTable.orderId].value,
-    paymentDate = this[PaymentTable.paymentDate],
-    amount = this[PaymentTable.amount],
-    method = this[PaymentTable.method]
+fun PaymentEntity.toPayment() = Payment(
+    id = id.value,
+    orderId = orderId.value,
+    paymentDate = paymentDate,
+    amount = amount,
+    method = method
 )
 
-fun ResultRow.toFavorite() = Favorite(
-    id = this[FavoriteTable.id].value,
-    userId = this[FavoriteTable.userId].value,
-    productId = this[FavoriteTable.productId].value
+fun FavoriteEntity.toFavorite() = Favorite(
+    id = id.value,
+    userId = userId.value,
+    productId = productId.value
+)
+
+fun AddressEntity.toAddress() = Address(
+    id = id.value,
+    name = name,
+    userId = userId.value,
+    address = address
 )

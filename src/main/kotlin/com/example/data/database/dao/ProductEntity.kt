f2f9ebec02d39a51a6732004c32cab44aad072b2
abdozcan <abdozcann@gmail.com>
2024-10-12 18:04:39 +0300

    edited ProductEntity and added reviews variable within it

--- src/main/kotlin/com/example/data/database/dao/ProductEntity.kt
+++ src/main/kotlin/com/example/data/database/dao/ProductEntity.kt
@@ -1,7 +1,7 @@
 package com.example.data.database.dao
 
-import com.example.data.database.table.CategoryTable
 import com.example.data.database.table.ProductTable
+import com.example.data.database.table.ReviewTable
 import org.jetbrains.exposed.dao.IntEntity
 import org.jetbrains.exposed.dao.IntEntityClass
 import org.jetbrains.exposed.dao.id.EntityID
@@ -12,17 +12,19 @@ class ProductEntity(id: EntityID<Int>) : IntEntity(id) {
 
     val name by ProductTable.name
     val description by ProductTable.description
-    val price  by ProductTable.price
-    val region  by ProductTable.region
+    val price by ProductTable.price
+    val region by ProductTable.region
     val weight by ProductTable.weight
     val flavorProfile by ProductTable.flavorProfile
     val grindOption by ProductTable.grindOption
     val roastLevel by ProductTable.roastLevel
     val imageUrl by ProductTable.imageUrl
-    val category by CategoryEntity referencedOn CategoryTable.name
+    val category by ProductTable.category
     val stockQuantity by ProductTable.stockQuantity
     val popularityRating by ProductTable.popularityRating
     val discountPrice by ProductTable.discountPrice
     val discountPercentage by ProductTable.discountPercentage
     val bestseller by ProductTable.bestseller
+
+    val reviews by ReviewEntity referrersOn ReviewTable.productId
 }

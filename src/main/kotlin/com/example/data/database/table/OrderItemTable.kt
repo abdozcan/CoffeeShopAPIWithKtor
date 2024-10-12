63d940bc3d76a99051cfde484d2dc770286b1068
abdozcan <abdozcann@gmail.com>
2024-10-12 15:32:00 +0300

    updated OrderItemTable

---
+++ src/main/kotlin/com/example/data/database/table/OrderItemTable.kt
@@ -0,0 +1,11 @@
+package com.example.data.database.table
+
+import org.jetbrains.exposed.dao.id.IntIdTable
+import org.jetbrains.exposed.sql.ReferenceOption
+
+object OrderItemTable : IntIdTable("order_items") {
+    val orderId = reference("order_id", foreign = OrderTable, onDelete = ReferenceOption.CASCADE)
+    val productId = reference("product_id", foreign = ProductTable)
+    val quantity = integer("quantity")
+    val price = float("price")
+}

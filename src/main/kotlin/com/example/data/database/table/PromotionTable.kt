7e3b92869f7d2c454134993f1da2b842b6e3e27c
abdozcan <abdozcann@gmail.com>
2024-10-12 15:32:21 +0300

    updated PromotionTable

--- src/main/kotlin/com/example/data/database/table/PromotionTable.kt
+++ src/main/kotlin/com/example/data/database/table/PromotionTable.kt
@@ -1,10 +1,11 @@
 package com.example.data.database.table
 
 import org.jetbrains.exposed.dao.id.IntIdTable
+import org.jetbrains.exposed.sql.ReferenceOption
 import org.jetbrains.exposed.sql.javatime.datetime
 
 object PromotionTable : IntIdTable("promotions") {
-    val productId = integer("product_id").references(ProductTable.id)
+    val productId = reference("product_id", foreign = ProductTable, onDelete = ReferenceOption.CASCADE)
     val discount = decimal("discount", 4, 2)
     val startDate = datetime("start_date")
     val endDate = datetime("end_date")

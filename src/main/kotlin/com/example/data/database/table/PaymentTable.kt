6fef98ebd84a2e713aa6ea7902374b09c37dd210
abdozcan <abdozcann@gmail.com>
2024-10-12 15:32:11 +0300

    updated PaymentTable

--- src/main/kotlin/com/example/data/database/table/PaymentTable.kt
+++ src/main/kotlin/com/example/data/database/table/PaymentTable.kt
@@ -1,10 +1,11 @@
 package com.example.data.database.table
 
 import org.jetbrains.exposed.dao.id.IntIdTable
+import org.jetbrains.exposed.sql.ReferenceOption
 import org.jetbrains.exposed.sql.javatime.datetime
 
 object PaymentTable : IntIdTable("payments") {
-    val orderId = integer("order_id").references(OrderTable.id)
+    val orderId = reference("order_id", foreign = OrderTable, onDelete = ReferenceOption.CASCADE)
     val paymentDate = datetime("payment_date")
     val amount = integer("amount")
     val method = text("method")

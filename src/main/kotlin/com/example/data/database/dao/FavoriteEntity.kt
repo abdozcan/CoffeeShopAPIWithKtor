deb62f8b5392de2423f464c75b207fa89c9e1701
abdozcan <abdozcann@gmail.com>
2024-10-12 18:03:20 +0300

    edited FavoriteEntity

--- src/main/kotlin/com/example/data/database/dao/FavoriteEntity.kt
+++ src/main/kotlin/com/example/data/database/dao/FavoriteEntity.kt
@@ -1,8 +1,6 @@
 package com.example.data.database.dao
 
 import com.example.data.database.table.FavoriteTable
-import com.example.data.database.table.ProductTable
-import com.example.data.database.table.UserTable
 import org.jetbrains.exposed.dao.IntEntity
 import org.jetbrains.exposed.dao.IntEntityClass
 import org.jetbrains.exposed.dao.id.EntityID
@@ -10,6 +8,6 @@ import org.jetbrains.exposed.dao.id.EntityID
 class FavoriteEntity(id: EntityID<Int>) : IntEntity(id) {
     companion object : IntEntityClass<FavoriteEntity>(FavoriteTable)
 
-    val userId by UserEntity referencedOn UserTable.id
-    val productId by ProductEntity referencedOn ProductTable.id
+    val userId by FavoriteTable.userId
+    val productId by FavoriteTable.productId
 }

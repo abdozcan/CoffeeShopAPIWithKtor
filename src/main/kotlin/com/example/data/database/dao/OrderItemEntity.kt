d442f38a6202e90d3c9da720e295914bfb2d743b
abdozcan <abdozcann@gmail.com>
2024-10-12 18:03:37 +0300

    edited OrderItemEntity

--- src/main/kotlin/com/example/data/database/dao/OrderItemEntity.kt
+++ src/main/kotlin/com/example/data/database/dao/OrderItemEntity.kt
@@ -1,8 +1,6 @@
 package com.example.data.database.dao
 
 import com.example.data.database.table.OrderItemTable
-import com.example.data.database.table.OrderTable
-import com.example.data.database.table.ProductTable
 import org.jetbrains.exposed.dao.IntEntity
 import org.jetbrains.exposed.dao.IntEntityClass
 import org.jetbrains.exposed.dao.id.EntityID
@@ -10,8 +8,8 @@ import org.jetbrains.exposed.dao.id.EntityID
 class OrderItemEntity(id: EntityID<Int>) : IntEntity(id) {
     companion object : IntEntityClass<OrderItemEntity>(OrderItemTable)
 
-    val orderId by OrderEntity referencedOn OrderTable.id
-    val productId by ProductEntity referencedOn ProductTable.id
+    val orderId by OrderItemTable.orderId
+    val productId by OrderItemTable.productId
     val quantity by OrderItemTable.quantity
     val price by OrderItemTable.price
 }

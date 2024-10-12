20db99fcbb183d4563655d9743a588ee96c0bb2c
abdozcan <abdozcann@gmail.com>
2024-10-12 15:32:05 +0300

    updated OrderTable

--- src/main/kotlin/com/example/data/database/table/OrderTable.kt
+++ src/main/kotlin/com/example/data/database/table/OrderTable.kt
@@ -1,10 +1,12 @@
 package com.example.data.database.table
 
+import com.example.data.model.User
 import org.jetbrains.exposed.dao.id.IntIdTable
+import org.jetbrains.exposed.sql.ReferenceOption
 import org.jetbrains.exposed.sql.javatime.datetime
 
 object OrderTable : IntIdTable("orders") {
-    val userId = integer("user_id").references(UserTable.id)
+    val userId = reference("user_id", foreign = UserTable)
     val orderDate = datetime("order_date")
     val amount = integer("amount")
     val status = text("status")

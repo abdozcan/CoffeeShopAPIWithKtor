e6a348081f267de62a80248558dd956b633cbd8c
abdozcan <abdozcann@gmail.com>
2024-10-13 22:03:39 +0300

    changed parameters of add function in OrderRepository

--- src/main/kotlin/com/example/domain/repository/OrderRepository.kt
+++ src/main/kotlin/com/example/domain/repository/OrderRepository.kt
@@ -1,11 +1,24 @@
 package com.example.domain.repository
 
 import com.example.domain.model.Order
+import java.time.LocalDateTime
 
 interface OrderRepository {
     suspend fun findAllByUserId(userId: Int): Result<List<Order>>
     suspend fun findById(id: Int): Result<Order>
-    suspend fun add(userId: Int, productId: Int): Result<Order>
     suspend fun edit(order: Order): Result<Unit>
     suspend fun delete(id: Int): Result<Unit>
+    suspend fun add(
+        userId: Int,
+        shippingAddress: String,
+        paymentMethod: String,
+        orderDate: LocalDateTime,
+        orderedProducts: List<OrderedProduct>
+    ): Result<Order>
 }
+
+data class OrderedProduct(
+    val id: Int,
+    val quantity: Int,
+    val price: Double,
+)

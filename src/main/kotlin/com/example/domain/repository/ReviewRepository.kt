8b7ef650702475a282eb5dd942f8214c978d8bb5
abdozcan <abdozcann@gmail.com>
2024-10-14 02:04:39 +0300

    changed parameter types

--- src/main/kotlin/com/example/domain/repository/ReviewRepository.kt
+++ src/main/kotlin/com/example/domain/repository/ReviewRepository.kt
@@ -5,7 +5,6 @@ import com.example.domain.model.Review
 interface ReviewRepository {
     suspend fun findAllByProductId(productId: Int): Result<List<Review>>
     suspend fun findAllByUserId(userId: Int): Result<List<Review>>
-    suspend fun add(userId: Int, productId: Int): Result<Review>
-    suspend fun edit(review: Review): Result<Unit>
+    suspend fun add(review: Review): Result<Review>
     suspend fun delete(id: Int): Result<Unit>
 }

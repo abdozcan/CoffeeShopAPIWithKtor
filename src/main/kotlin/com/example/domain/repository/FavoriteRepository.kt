a1fd729b12bbdf0936540174baac61d50f1fa4ce
abdozcan <abdozcann@gmail.com>
2024-10-13 22:02:49 +0300

    changed return type of isFavorite function

--- src/main/kotlin/com/example/domain/repository/FavoriteRepository.kt
+++ src/main/kotlin/com/example/domain/repository/FavoriteRepository.kt
@@ -3,7 +3,7 @@ package com.example.domain.repository
 import com.example.domain.model.Favorite
 
 interface FavoriteRepository {
-    suspend fun isFavorite(userId: Int, productId: Int): Result<Boolean>
+    suspend fun isFavorite(userId: Int, productId: Int): Result<Int>
     suspend fun add(userId: Int, productId: Int): Result<Int>
     suspend fun delete(id: Int): Result<Unit>
 }

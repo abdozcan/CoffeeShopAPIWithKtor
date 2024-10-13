4690728a41139659dca26b10b9a60dea7654ce0f
abdozcan <abdozcann@gmail.com>
2024-10-13 22:04:20 +0300

    created OrderStatus enum class

---
+++ src/main/kotlin/com/example/data/utils/OrderStatus.kt
@@ -0,0 +1,18 @@
+package com.example.data.utils
+
+enum class OrderStatus {
+    PENDING,
+    CONFIRMED,
+    PROCESSING,
+    SHIPPED,
+    OUT_FOR_DELIVERY,
+    DELIVERED,
+    CANCELLED,
+    FAILED,
+    RETURNED,
+    REFUNDED,
+    ON_HOLD,
+    PARTIALLY_SHIPPED
+}
+
+

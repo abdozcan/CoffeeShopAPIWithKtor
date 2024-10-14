df4b4ffeee86bf819e42506b8afa96f000507d63
abdozcan <abdozcann@gmail.com>
2024-10-14 21:05:51 +0300

    implemented ReviewRepository

---
+++ src/main/kotlin/com/example/data/repository/DefaultReviewRepository.kt
@@ -0,0 +1,52 @@
+package com.example.data.repository
+
+import com.example.data.database.dao.ReviewEntity
+import com.example.data.database.table.ProductTable
+import com.example.data.database.table.ReviewTable
+import com.example.data.database.table.UserTable
+import com.example.data.utils.doOrThrowIfNull
+import com.example.data.utils.mapOrTrowIfEmpty
+import com.example.data.utils.withTransactionContext
+import com.example.domain.model.Review
+import com.example.domain.repository.ReviewRepository
+import org.jetbrains.exposed.dao.id.EntityID
+
+class DefaultReviewRepository : ReviewRepository {
+    override suspend fun findAllByProductId(productId: Int): Result<List<Review>> = runCatching {
+        withTransactionContext {
+            ReviewEntity.find {
+                ReviewTable.productId eq productId
+            }.mapOrTrowIfEmpty {
+                it.toReview()
+            }
+        }
+    }
+
+    override suspend fun findAllByUserId(userId: Int): Result<List<Review>> = runCatching {
+        withTransactionContext {
+            ReviewEntity.find {
+                ReviewTable.userId eq userId
+            }.map {
+                it.toReview()
+            }
+        }
+    }
+
+    override suspend fun add(review: Review): Result<Review> = runCatching {
+        withTransactionContext {
+            ReviewEntity.new {
+                this.productId = EntityID(review.productId, ProductTable)
+                this.userId = EntityID(review.userId, UserTable)
+                this.rating = review.rating
+                this.comment = review.comment
+                this.reviewDate = review.reviewDate
+            }.toReview()
+        }
+    }
+
+    override suspend fun delete(id: Int): Result<Unit> = runCatching {
+        withTransactionContext {
+            ReviewEntity.findById(id).doOrThrowIfNull { it.delete() }
+        }
+    }
+}

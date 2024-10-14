2ba1ad1f5243ac5a4821fa3c5d3ea1964aa9ef06
abdozcan <abdozcann@gmail.com>
2024-10-14 21:49:20 +0300

    implemented finAllByUserId function in DefaultUserRepository

--- src/main/kotlin/com/example/data/repository/DefaultFavoriteRepository.kt
+++ src/main/kotlin/com/example/data/repository/DefaultFavoriteRepository.kt
@@ -5,12 +5,24 @@ import com.example.data.database.table.FavoriteTable
 import com.example.data.database.table.ProductTable
 import com.example.data.database.table.UserTable
 import com.example.data.utils.doOrThrowIfNull
+import com.example.data.utils.mapOrTrowIfEmpty
 import com.example.data.utils.withTransactionContext
+import com.example.domain.model.Favorite
 import com.example.domain.repository.FavoriteRepository
 import org.jetbrains.exposed.dao.id.EntityID
 import org.jetbrains.exposed.sql.and
 
 class DefaultFavoriteRepository : FavoriteRepository {
+    override suspend fun finAllByUserId(userId: Int): Result<List<Favorite>> = runCatching {
+        withTransactionContext {
+            FavoriteEntity.find {
+                FavoriteTable.userId eq userId
+            }.mapOrTrowIfEmpty {
+                it.toFavorite()
+            }
+        }
+    }
+
 
     override suspend fun isFavorite(userId: Int, productId: Int): Result<Int> = runCatching {
         withTransactionContext {

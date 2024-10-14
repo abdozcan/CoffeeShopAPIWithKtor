d9f25c09c781e1c1158ebf928ac2ccf746eb6e55
abdozcan <abdozcann@gmail.com>
2024-10-14 21:05:20 +0300

    implemented FavoriteRepository

---
+++ src/main/kotlin/com/example/data/repository/DefaultFavoriteRepository.kt
@@ -0,0 +1,38 @@
+package com.example.data.repository
+
+import com.example.data.database.dao.FavoriteEntity
+import com.example.data.database.table.FavoriteTable
+import com.example.data.database.table.ProductTable
+import com.example.data.database.table.UserTable
+import com.example.data.utils.doOrThrowIfNull
+import com.example.data.utils.withTransactionContext
+import com.example.domain.repository.FavoriteRepository
+import org.jetbrains.exposed.dao.id.EntityID
+import org.jetbrains.exposed.sql.and
+
+class DefaultFavoriteRepository : FavoriteRepository {
+
+    override suspend fun isFavorite(userId: Int, productId: Int): Result<Int> = runCatching {
+        withTransactionContext {
+            FavoriteEntity
+                .find {
+                    (FavoriteTable.userId eq userId) and (FavoriteTable.productId eq productId)
+                }.single().id.value
+        }
+    }
+
+    override suspend fun add(userId: Int, productId: Int): Result<Int> = runCatching {
+        withTransactionContext {
+            FavoriteEntity.new {
+                this.userId = EntityID(userId, UserTable)
+                this.productId = EntityID(productId, ProductTable)
+            }.id.value
+        }
+    }
+
+    override suspend fun delete(id: Int): Result<Unit> = runCatching {
+        withTransactionContext {
+            FavoriteEntity.findById(id).doOrThrowIfNull { it.delete() }
+        }
+    }
+}

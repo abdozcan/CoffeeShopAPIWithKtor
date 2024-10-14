0869cdad15db5002f60e9832b9e4487a2968626f
abdozcan <abdozcann@gmail.com>
2024-10-14 21:50:49 +0300

    named selectAddress method to selectDefaultAddress and changed its addressId: Int parameter as address: String

--- src/main/kotlin/com/example/domain/repository/UserRepository.kt
+++ src/main/kotlin/com/example/domain/repository/UserRepository.kt
@@ -1,11 +1,10 @@
 package com.example.domain.repository
 
-import com.example.domain.model.Address
 import com.example.domain.model.User
 
 interface UserRepository {
     suspend fun findByEmail(email: String): Result<User>
     suspend fun add(name: String, password: String, email: String, phone: String, address: String = ""): Result<User>
     suspend fun delete(id: Int): Result<Unit>
-    suspend fun selectAddress(userId: Int, addressId: Int): Result<Address>
+    suspend fun selectDefaultAddress(userId: Int, address: String): Result<User>
 }

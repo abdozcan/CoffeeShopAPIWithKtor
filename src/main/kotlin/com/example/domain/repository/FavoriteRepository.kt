7c98047c7eb5be75f2120b462f59c1177b47e215
abdozcan <abdozcann@gmail.com>
2024-10-14 21:48:54 +0300

    added finAllByUserId function to UserRepository

--- src/main/kotlin/com/example/domain/repository/FavoriteRepository.kt
+++ src/main/kotlin/com/example/domain/repository/FavoriteRepository.kt
@@ -3,6 +3,7 @@ package com.example.domain.repository
 import com.example.domain.model.Favorite
 
 interface FavoriteRepository {
+    suspend fun finAllByUserId(userId: Int): Result<List<Favorite>>
     suspend fun isFavorite(userId: Int, productId: Int): Result<Int>
     suspend fun add(userId: Int, productId: Int): Result<Int>
     suspend fun delete(id: Int): Result<Unit>

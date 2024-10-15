899d74d78acf620a0f9ff879f949c6ac4e2a3aae
abdozcan <abdozcann@gmail.com>
2024-10-15 22:18:59 +0300

    added expiresAt, status, isExpired properties and implemented logic to adjust prices based on expiration -

--- src/main/kotlin/com/example/data/database/dao/CartItemEntity.kt
+++ src/main/kotlin/com/example/data/database/dao/CartItemEntity.kt
@@ -6,6 +6,7 @@ import com.example.domain.model.CartItem
 import org.jetbrains.exposed.dao.IntEntity
 import org.jetbrains.exposed.dao.IntEntityClass
 import org.jetbrains.exposed.dao.id.EntityID
+import java.time.LocalDateTime
 
 class CartItemEntity(id: EntityID<Int>) : IntEntity(id) {
     companion object : IntEntityClass<CartItemEntity>(CartItemTable)
@@ -16,14 +17,21 @@ class CartItemEntity(id: EntityID<Int>) : IntEntity(id) {
     var userId by CartItemTable.userId
     var discountPrice by CartItemTable.discountPrice
     var discountPercentage by CartItemTable.discountPercentage
+    var expiresAt by CartItemTable.expiresAt
+    var status by CartItemTable.status
+
+    private val isExpired: Boolean
+        get() = LocalDateTime.now().isAfter(expiresAt)
 
     fun toCartItem() = CartItem(
         id = id.value,
         userId = userId.value,
         product = product.toProduct(),
         quantity = quantity,
-        price = price,
-        discountPrice = discountPrice,
-        discountPercentage = discountPercentage
+        price = if (isExpired) product.price else price,
+        discountPrice = if (isExpired) product.discountPrice else discountPrice,
+        discountPercentage = if (isExpired) product.discountPercentage else discountPercentage,
+        expiresAt = expiresAt,
+        status = status
     )
 }

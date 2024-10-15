e25f88f413b4ab359946a7ee93da60a0238255e8
abdozcan <abdozcann@gmail.com>
2024-10-15 20:37:49 +0300

    created CartRepository

---
+++ src/main/kotlin/com/example/domain/repository/CartRepository.kt
@@ -0,0 +1,9 @@
+package com.example.domain.repository
+
+import com.example.domain.model.CartItem
+
+interface CartRepository {
+    suspend fun findByUserId(userId: Int): Result<List<CartItem>>
+    suspend fun add(userId: Int, productId: Int, quantity: Int): Result<Unit>
+    suspend fun delete(ids: List<Int>): Result<Unit>
+}

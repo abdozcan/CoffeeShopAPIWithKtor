b08a7d87843a2770130810c79f70bcc43f967684
abdozcan <abdozcann@gmail.com>
2024-10-15 20:38:03 +0300

    implemented CartRepository

---
+++ src/main/kotlin/com/example/data/repository/DefaultCartRepository.kt
@@ -0,0 +1,39 @@
+package com.example.data.repository
+
+import com.example.data.database.dao.CartItemEntity
+import com.example.data.database.dao.ProductEntity
+import com.example.data.database.table.CartItemTable
+import com.example.data.database.table.UserTable
+import com.example.data.utils.doOrThrowIfNull
+import com.example.data.utils.withTransactionContext
+import com.example.domain.model.CartItem
+import com.example.domain.repository.CartRepository
+import org.jetbrains.exposed.dao.id.EntityID
+
+class DefaultCartRepository : CartRepository {
+    override suspend fun findByUserId(userId: Int): Result<List<CartItem>> = runCatching {
+        withTransactionContext {
+            CartItemEntity.find {
+                CartItemTable.userId eq userId
+            }.map {
+                it.toCartItem()
+            }
+        }
+    }
+
+    override suspend fun add(userId: Int, productId: Int, quantity: Int): Result<Unit> = runCatching {
+        withTransactionContext {
+            CartItemEntity.new {
+                this.userId = EntityID(userId, UserTable)
+                this.product = ProductEntity.findById(productId).doOrThrowIfNull { it }
+                this.quantity = quantity
+            }
+        }
+    }
+
+    override suspend fun delete(ids: List<Int>): Result<Unit> = runCatching {
+        withTransactionContext {
+            CartItemEntity.forIds(ids).map { it.delete() }
+        }
+    }
+}
--- src/main/kotlin/com/example/data/utils/withTransactionContext.kt
+++ src/main/kotlin/com/example/data/utils/withTransactionContext.kt
@@ -4,5 +4,7 @@ import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
 import org.jetbrains.exposed.sql.transactions.experimental.newSuspendedTransaction
 
-suspend fun <T> withTransactionContext(dispatcher: CoroutineDispatcher = Dispatchers.IO, block: suspend () -> T): T =
-    newSuspendedTransaction(dispatcher) { block() }
+suspend fun <T> withTransactionContext(
+    dispatcher: CoroutineDispatcher = Dispatchers.IO,
+    block: suspend () -> T
+): T = newSuspendedTransaction(dispatcher) { block() }

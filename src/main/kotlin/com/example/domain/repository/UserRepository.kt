dde4d3e2c5d2f901f7cac213c5f46429687ec2fe
abdozcan <abdozcann@gmail.com>
2024-10-16 22:10:30 +0300

    added isEmailUsed, isPhoneUsed and isIncorrectCredential methods to UserRepository

--- src/main/kotlin/com/example/domain/repository/UserRepository.kt
+++ src/main/kotlin/com/example/domain/repository/UserRepository.kt
@@ -1,10 +1,20 @@
 package com.example.domain.repository
 
 import com.example.domain.model.User
+import com.example.routes.Credential
 
 interface UserRepository {
     suspend fun findByEmail(email: String): Result<User>
-    suspend fun add(name: String, password: String, email: String, phone: String, address: String = ""): Result<User>
+    suspend fun isEmailUsed(email: String): Boolean
+    suspend fun isPhoneUsed(phone: String): Boolean
+    suspend fun isIncorrectCredential(credential: Credential): Boolean
+    suspend fun add(
+        name: String,
+        password: String,
+        email: String,
+        phone: String,
+        defaultAddress: String = ""
+    ): Result<User>
     suspend fun delete(id: Int): Result<Unit>
     suspend fun selectDefaultAddress(userId: Int, address: String): Result<User>
 }

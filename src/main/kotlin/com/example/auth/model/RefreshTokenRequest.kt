581f6470fc98d673f12ed7b85b246262466b202d
abdozcan <abdozcann@gmail.com>
2024-10-16 22:45:18 +0300

    moved RefreshTokenRequest data class

---
+++ src/main/kotlin/com/example/auth/model/RefreshTokenRequest.kt
@@ -0,0 +1,13 @@
+package com.example.auth.model
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class RefreshTokenRequest(
+    val email: String,
+    @SerialName("access_token")
+    val accessToken: String,
+    @SerialName("refresh_token")
+    val refreshToken: String
+)

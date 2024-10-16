0b9d9f016c7c44915ba4752b0e0648194b3aa043
abdozcan <abdozcann@gmail.com>
2024-10-16 22:07:55 +0300

    added doc comment to a function that throws an exception

--- src/main/kotlin/com/example/domain/repository/ProductRepository.kt
+++ src/main/kotlin/com/example/domain/repository/ProductRepository.kt
@@ -5,6 +5,11 @@ import com.example.domain.model.Product
 interface ProductRepository {
     suspend fun all(): Result<List<Product>>
     suspend fun findById(id: Int): Result<Product>
+
+
+    /**
+     * @throws NotFoundException
+     */
     suspend fun findByCategory(category: String): Result<List<Product>>
     suspend fun findBestsellers(): Result<List<Product>>
     suspend fun findFavoritedProduct(userId: Int): Result<List<Product>>

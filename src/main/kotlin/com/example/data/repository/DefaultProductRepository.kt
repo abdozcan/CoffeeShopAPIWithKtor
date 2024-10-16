7bc3111a9f5d8fecb9f10b5da4c9402784138893
abdozcan <abdozcann@gmail.com>
2024-10-16 22:04:58 +0300

    added doc comment for functions that throws an exception and used mapOrThrowIfEmpty extension function instead of map

--- src/main/kotlin/com/example/data/repository/DefaultProductRepository.kt
+++ src/main/kotlin/com/example/data/repository/DefaultProductRepository.kt
@@ -4,8 +4,9 @@ import com.example.data.database.dao.FavoriteEntity
 import com.example.data.database.dao.ProductEntity
 import com.example.data.database.table.FavoriteTable
 import com.example.data.database.table.ProductTable
-import com.example.data.utils.flatMapOrTrowIfEmpty
 import com.example.data.utils.doOrThrowIfNull
+import com.example.data.utils.flatMapOrTrowIfEmpty
+import com.example.data.utils.mapOrTrowIfEmpty
 import com.example.data.utils.withTransactionContext
 import com.example.domain.model.Product
 import com.example.domain.repository.ProductRepository
@@ -13,21 +14,27 @@ import com.example.domain.repository.ProductRepository
 class DefaultProductRepository : ProductRepository {
     override suspend fun all(): Result<List<Product>> = runCatching {
         withTransactionContext {
-            ProductEntity.all().map { it.toProduct() }
+            ProductEntity.all().mapOrTrowIfEmpty { it.toProduct() }
         }
     }
 
+    /**
+     * @throws NotFoundException
+     */
     override suspend fun findById(id: Int): Result<Product> = runCatching {
         withTransactionContext {
             ProductEntity.findById(id).doOrThrowIfNull { it.toProduct() }
         }
     }
 
+    /**
+     * @throws NoSuchElementException
+     */
     override suspend fun findByCategory(category: String): Result<List<Product>> = runCatching {
         withTransactionContext {
             ProductEntity.find {
                 ProductTable.category eq category
-            }.map {
+            }.mapOrTrowIfEmpty {
                 it.toProduct()
             }
         }
@@ -37,7 +44,7 @@ class DefaultProductRepository : ProductRepository {
         withTransactionContext {
             ProductEntity.find {
                 ProductTable.bestseller eq true
-            }.map {
+            }.mapOrTrowIfEmpty {
                 it.toProduct()
             }
         }

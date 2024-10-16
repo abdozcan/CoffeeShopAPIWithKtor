c79629c163d3bd464ff0c8e9b33862e649de3684
abdozcan <abdozcann@gmail.com>
2024-10-16 22:03:17 +0300

    added doc comment for functions that throws an exception

--- src/main/kotlin/com/example/data/utils/Extention.kt
+++ src/main/kotlin/com/example/data/utils/Extention.kt
@@ -2,10 +2,16 @@ package com.example.data.utils
 
 import io.ktor.server.plugins.*
 
+/**
+ * @throws NotFoundException
+ */
 inline fun <T, R: Any> T?.doOrThrowIfNull(transform: (T) -> R): R {
     return this?.let(transform) ?: throw NotFoundException("Not Found.")
 }
 
+/**
+ * @throws NoSuchElementException
+ */
 inline fun <T, R> Iterable<T>.mapOrTrowIfEmpty(transform: (T) -> R): List<R> {
     if (this.none()) {
         throw NoSuchElementException("The collection is empty.")

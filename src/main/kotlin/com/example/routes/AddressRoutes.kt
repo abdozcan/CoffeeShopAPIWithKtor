0d08d818a63bb7bc1fd69b003aca8574e398b9a8
abdozcan <abdozcann@gmail.com>
2024-10-19 23:18:56 +0300

    created addressRoutes and moved two functions within userRoutes to addressRoutes

---
+++ src/main/kotlin/com/example/routes/AddressRoutes.kt
@@ -0,0 +1,76 @@
+package com.example.routes
+
+import com.example.data.utils.Constant.ADDRESS_LENGTH
+import com.example.data.utils.Constant.ADDRESS_NAME_LENGTH
+import com.example.domain.model.Address
+import com.example.domain.repository.AddressRepository
+import io.ktor.http.*
+import io.ktor.server.application.*
+import io.ktor.server.auth.*
+import io.ktor.server.plugins.*
+import io.ktor.server.request.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+fun Application.addressRoutes(addressRepo: AddressRepository) = routing {
+    authentication {
+        findAllByUserId(addressRepo)
+        delete(addressRepo)
+        add(addressRepo)
+        edit(addressRepo)
+    }
+}
+
+private fun Route.findAllByUserId(repo: AddressRepository) = get("address/user/{userId?}") {
+    call.parameters["userId"]?.toInt()?.let { userId ->
+        repo.findAllByUserId(userId).getOrThrow().let { addressList ->
+            call.respond(addressList)
+        }
+    } ?: throw MissingRequestParameterException("user ID")
+}
+
+private fun Route.add(repo: AddressRepository) = post("address/add") {
+    call.receive<AddAddressRequest>().let { address ->
+        if (address.name.length > ADDRESS_NAME_LENGTH)
+            return@post call.respond(
+                HttpStatusCode.BadRequest,
+                "Address name length must be less than $ADDRESS_NAME_LENGTH."
+            )
+
+        if (address.address.length > ADDRESS_LENGTH)
+            return@post call.respond(
+                HttpStatusCode.BadRequest,
+                "Address length must be less than $ADDRESS_LENGTH."
+            )
+
+        repo.add(address.name, address.userId, address.address).getOrThrow().let {
+            call.respond("Address added successfully.")
+        }
+    }
+}
+
+private fun Route.delete(repo: AddressRepository) = delete("address/delete/{id?}") {
+    call.parameters["id"]?.toInt()?.let { id ->
+        repo.delete(id).getOrThrow().let {
+            call.respond("Address deleted successfully.")
+        }
+    } ?: throw MissingRequestParameterException("ID")
+}
+
+private fun Route.edit(repo: AddressRepository) = put("order/edit") {
+    call.receive<Address>().let { address ->
+        repo.edit(address).getOrThrow().let {
+            call.respond("Address edited successfully.")
+        }
+    }
+}
+
+@Serializable
+data class AddAddressRequest(
+    val name: String,
+    @SerialName("user_id")
+    val userId: Int,
+    val address: String
+)
--- src/main/kotlin/com/example/routes/UserRoutes.kt
+++ src/main/kotlin/com/example/routes/UserRoutes.kt
@@ -1,8 +1,6 @@
 package com.example.routes
 
 import com.example.data.utils.Constant.ADDRESS_LENGTH
-import com.example.data.utils.Constant.ADDRESS_NAME_LENGTH
-import com.example.domain.repository.AddressRepository
 import com.example.domain.repository.UserRepository
 import io.ktor.http.*
 import io.ktor.server.application.*
@@ -13,12 +11,10 @@ import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import kotlinx.serialization.Serializable
 
-fun Application.userRoutes(userRepo: UserRepository, addressRepo: AddressRepository) = routing {
+fun Application.userRoutes(userRepo: UserRepository) = routing {
     authenticate {
         getByEmail(userRepo)
-        getAddresses(addressRepo)
         setDefaultAddress(userRepo)
-        addAddress(addressRepo)
         delete(userRepo)
     }
 }
@@ -31,15 +27,7 @@ fun Route.getByEmail(repo: UserRepository) = get("/users/{email?}") {
     } ?: throw MissingRequestParameterException("email")
 }
 
-fun Route.getAddresses(addressRepo: AddressRepository) = get("/users/{userId?}/addresses") {
-    call.parameters["userID"]?.toInt()?.let { userId ->
-        addressRepo.findAllByUserId(userId).getOrThrow().let { addressList ->
-                call.respond(HttpStatusCode.OK, addressList)
-            }
-    } ?: throw MissingRequestParameterException("user ID")
-}
-
-fun Route.setDefaultAddress(repo: UserRepository) = post("/users/{userId?}/default_address/set") {
+fun Route.setDefaultAddress(repo: UserRepository) = post("/user/{userId?}/default-address") {
     call.parameters["userId"]?.toInt()?.let { userId ->
         call.receiveText().let { address ->
             if (address.length > ADDRESS_LENGTH)
@@ -48,38 +36,9 @@ fun Route.setDefaultAddress(repo: UserRepository) = post("/users/{userId?}/defau
                     "Address length must be less than $ADDRESS_LENGTH."
                 )
 
-            repo.selectDefaultAddress(
-                userId,
-                address
-            ).getOrThrow().let { user ->
-                    call.respond(HttpStatusCode.OK, user)
-                }
-        }
-    } ?: throw MissingRequestParameterException("user ID")
-}
-
-fun Route.addAddress(addressRepo: AddressRepository) = post("/users/{userId?}/address/add") {
-    call.parameters["userId"]?.toInt()?.let { userId ->
-        call.receive<AddressRequest>().let { addressRequest ->
-            if (addressRequest.name.length > ADDRESS_NAME_LENGTH)
-                return@post call.respond(
-                    HttpStatusCode.BadRequest,
-                    "Address name length must be less than $ADDRESS_NAME_LENGTH."
-                )
-
-            if (addressRequest.address.length > ADDRESS_LENGTH)
-                return@post call.respond(
-                    HttpStatusCode.BadRequest,
-                    "Address length must be less than $ADDRESS_LENGTH."
-                )
-
-            addressRepo.add(
-                addressRequest.name,
-                userId,
-                addressRequest.address
-            ).getOrThrow().let { address ->
-                    call.respond(HttpStatusCode.OK, address)
-                }
+            repo.selectDefaultAddress(userId, address).getOrThrow().let { user ->
+                call.respond(HttpStatusCode.OK, user)
+            }
         }
     } ?: throw MissingRequestParameterException("user ID")
 }
@@ -92,7 +51,7 @@ fun Route.delete(repo: UserRepository) = delete("/users/{id?}") {
 }
 
 @Serializable
-private data class AddressRequest(
+data class AddressRequest(
     val name: String,
     val address: String
 )

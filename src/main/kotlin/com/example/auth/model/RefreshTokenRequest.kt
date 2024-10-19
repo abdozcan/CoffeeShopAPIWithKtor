800730b0453dfe2bd29a8564bab4ae65fc2f5437
abdozcan <abdozcann@gmail.com>
2024-10-19 17:21:51 +0300

    removed access_token property from RefreshTokenRequest data class

--- src/main/kotlin/com/example/auth/model/RefreshTokenRequest.kt
+++ src/main/kotlin/com/example/auth/model/RefreshTokenRequest.kt
@@ -6,8 +6,6 @@ import kotlinx.serialization.Serializable
 @Serializable
 data class RefreshTokenRequest(
     val email: String,
-    @SerialName("access_token")
-    val accessToken: String,
     @SerialName("refresh_token")
     val refreshToken: String
 )

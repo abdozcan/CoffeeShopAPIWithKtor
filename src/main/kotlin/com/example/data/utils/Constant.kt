042dd43b4ce6f80f940e48ef4d503bd96e31c3d9
abdozcan <abdozcann@gmail.com>
2024-10-19 17:22:25 +0300

    set ACCESS_TOKEN_EXPIRATION_MILLISECOND to 24 hours

--- src/main/kotlin/com/example/data/utils/Constant.kt
+++ src/main/kotlin/com/example/data/utils/Constant.kt
@@ -2,7 +2,7 @@ package com.example.data.utils
 
 object Constant {
     const val CART_ITEM_EXPIRATION_HOURS: Long = 24
-    const val ACCESS_TOKEN_EXPIRATION_MILLISECOND: Long = 60000
+    const val ACCESS_TOKEN_EXPIRATION_MILLISECOND: Long = 60000 * 60
     const val ADDRESS_LENGTH = 255
     const val ADDRESS_NAME_LENGTH = 50
     const val COMMENT_LENGTH = 500

a142c16da7261132fb20485881c513f67043e347
abdozcan <abdozcann@gmail.com>
2024-10-19 23:12:49 +0300

    added edit function to ReviewRepository

--- src/main/kotlin/com/example/domain/repository/ReviewRepository.kt
+++ src/main/kotlin/com/example/domain/repository/ReviewRepository.kt
@@ -5,6 +5,7 @@ import com.example.domain.model.Review
 interface ReviewRepository {
     suspend fun findAllByProductId(productId: Int): Result<List<Review>>
     suspend fun findAllByUserId(userId: Int): Result<List<Review>>
-    suspend fun add(review: Review): Result<Review>
+    suspend fun add(review: Review): Result<Unit>
+    suspend fun edit(review: Review): Result<Unit>
     suspend fun delete(id: Int): Result<Unit>
 }

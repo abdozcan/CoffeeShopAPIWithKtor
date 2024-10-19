dbdf5146dff19b4e94cde250b327038730e36f74
abdozcan <abdozcann@gmail.com>
2024-10-19 23:09:09 +0300

    fixed typo "finAllByUserId"

--- src/main/kotlin/com/example/domain/repository/FavoriteRepository.kt
+++ src/main/kotlin/com/example/domain/repository/FavoriteRepository.kt
@@ -3,8 +3,8 @@ package com.example.domain.repository
 import com.example.domain.model.Favorite
 
 interface FavoriteRepository {
-    suspend fun finAllByUserId(userId: Int): Result<List<Favorite>>
-    suspend fun isFavorite(userId: Int, productId: Int): Result<Int>
-    suspend fun add(userId: Int, productId: Int): Result<Int>
-    suspend fun delete(id: Int): Result<Unit>
+    suspend fun findAllByUserId(userId: Int): Result<List<Favorite>>
+    suspend fun isFavorite(userId: Int, productId: Int): Result<Boolean>
+    suspend fun add(userId: Int, productId: Int): Result<Unit>
+    suspend fun delete(productId: Int): Result<Unit>
 }

1059eb1c503ae7075ad79c732c897ac0dd18b178
abdozcan <abdozcann@gmail.com>
2024-10-19 23:07:37 +0300

    removed "users" property

--- src/main/kotlin/com/example/data/database/dao/ReviewEntity.kt
+++ src/main/kotlin/com/example/data/database/dao/ReviewEntity.kt
@@ -1,7 +1,6 @@
 package com.example.data.database.dao
 
 import com.example.data.database.table.ReviewTable
-import com.example.data.database.table.UserTable
 import com.example.domain.model.Review
 import org.jetbrains.exposed.dao.IntEntity
 import org.jetbrains.exposed.dao.IntEntityClass
@@ -15,8 +14,6 @@ class ReviewEntity(id: EntityID<Int>) : IntEntity(id) {
     var rating by ReviewTable.rating
     var comment by ReviewTable.comment
     var reviewDate by ReviewTable.reviewDate
-
-    val users by UserEntity referrersOn UserTable.id
 
     fun toReview() = Review(
         id = id.value,

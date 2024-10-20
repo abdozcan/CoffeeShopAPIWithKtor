d9ffd9531312db1eb409864944dad54629b8782f
abdozcan <abdozcann@gmail.com>
2024-10-20 20:41:38 +0300

    replaced `refColumn = UserTable.id` with `foreign = UserTable`

--- src/main/kotlin/com/example/data/database/table/AddressTable.kt
+++ src/main/kotlin/com/example/data/database/table/AddressTable.kt
@@ -7,6 +7,6 @@ import org.jetbrains.exposed.sql.ReferenceOption
 
 object AddressTable : IntIdTable("addresses") {
     val name = varchar("name", ADDRESS_NAME_LENGTH)
-    val userId = reference("user_id", refColumn = UserTable.id, onDelete = ReferenceOption.CASCADE)
+    val userId = reference("user_id", foreign = UserTable, onDelete = ReferenceOption.CASCADE)
     val address = varchar("address", ADDRESS_LENGTH)
 }

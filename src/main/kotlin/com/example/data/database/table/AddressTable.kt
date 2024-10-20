97f37b3d5625ff71b72d9383ee4cbbc1c7897e87
abdozcan <abdozcann@gmail.com>
2024-10-20 17:03:29 +0300

    removed being unique from name field

--- src/main/kotlin/com/example/data/database/table/AddressTable.kt
+++ src/main/kotlin/com/example/data/database/table/AddressTable.kt
@@ -6,7 +6,7 @@ import org.jetbrains.exposed.dao.id.IntIdTable
 import org.jetbrains.exposed.sql.ReferenceOption
 
 object AddressTable : IntIdTable("addresses") {
-    val name = varchar("name", ADDRESS_NAME_LENGTH).uniqueIndex()
+    val name = varchar("name", ADDRESS_NAME_LENGTH)
     val userId = reference("user_id", refColumn = UserTable.id, onDelete = ReferenceOption.CASCADE)
     val address = varchar("address", ADDRESS_LENGTH)
 }

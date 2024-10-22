dfc243621c1128ff532eebefef2519401df2f09d
abdozcan <abdozcann@gmail.com>
2024-10-22 22:14:15 +0300

    set `userId`s onDelete to ReferenceOption.CASCADE

--- src/main/kotlin/com/example/data/database/table/PromotionTable.kt
+++ src/main/kotlin/com/example/data/database/table/PromotionTable.kt
@@ -1,10 +1,11 @@
 package com.example.data.database.table
 
 import org.jetbrains.exposed.dao.id.IntIdTable
+import org.jetbrains.exposed.sql.ReferenceOption
 import org.jetbrains.exposed.sql.javatime.datetime
 
 object PromotionTable : IntIdTable("promotions") {
-    val productId = reference("product_id", foreign = ProductTable)
+    val productId = reference("product_id", foreign = ProductTable, onDelete = ReferenceOption.CASCADE)
     val discount = float("discount")
     val startDate = datetime("start_date")
     val endDate = datetime("end_date")

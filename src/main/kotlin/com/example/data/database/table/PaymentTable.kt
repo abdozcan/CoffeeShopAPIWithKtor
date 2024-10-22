22d665bde4ed111086e143a5bb692a1e9fd2d6a7
abdozcan <abdozcann@gmail.com>
2024-10-22 22:14:27 +0300

    set `userId`s onDelete to ReferenceOption.NO_ACTION

--- src/main/kotlin/com/example/data/database/table/PaymentTable.kt
+++ src/main/kotlin/com/example/data/database/table/PaymentTable.kt
@@ -1,10 +1,11 @@
 package com.example.data.database.table
 
 import org.jetbrains.exposed.dao.id.IntIdTable
+import org.jetbrains.exposed.sql.ReferenceOption
 import org.jetbrains.exposed.sql.javatime.datetime
 
 object PaymentTable : IntIdTable("payments") {
-    val orderId = reference("order_id", foreign = OrderTable)
+    val orderId = reference("order_id", foreign = OrderTable, onDelete = ReferenceOption.NO_ACTION)
     val paymentDate = datetime("payment_date")
     val amount = integer("amount")
     val method = text("method")

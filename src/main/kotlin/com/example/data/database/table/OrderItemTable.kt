4e14531e709c0b4718e1cc8512df4f2efdfd222d
abdozcan <abdozcann@gmail.com>
2024-10-22 22:15:07 +0300

    set `userId` and `productId`s onDelete and onUpdate parameters to ReferenceOption.NO_ACTION

--- src/main/kotlin/com/example/data/database/table/OrderItemTable.kt
+++ src/main/kotlin/com/example/data/database/table/OrderItemTable.kt
@@ -4,8 +4,18 @@ import org.jetbrains.exposed.dao.id.IntIdTable
 import org.jetbrains.exposed.sql.ReferenceOption
 
 object OrderItemTable : IntIdTable("order_items") {
-    val orderId = reference("order_id", foreign = OrderTable, onDelete = ReferenceOption.CASCADE)
-    val productId = reference("product_id", foreign = ProductTable)
+    val orderId = reference(
+        "order_id",
+        foreign = OrderTable,
+        onDelete = ReferenceOption.NO_ACTION,
+        onUpdate = ReferenceOption.NO_ACTION
+    )
+    val productId = reference(
+        "product_id",
+        foreign = ProductTable,
+        onDelete = ReferenceOption.NO_ACTION,
+        onUpdate = ReferenceOption.NO_ACTION
+    )
     val quantity = integer("quantity")
     val price = double("price")
 }

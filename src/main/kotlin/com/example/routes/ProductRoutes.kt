a75b5b1aca62ea3371b9c83e25cafd65f8216e28
abdozcan <abdozcann@gmail.com>
2024-10-25 23:10:49 +0300

    created search route

--- src/main/kotlin/com/example/routes/ProductRoutes.kt
+++ src/main/kotlin/com/example/routes/ProductRoutes.kt
@@ -1,10 +1,12 @@
 package com.example.routes
 
 import com.example.domain.model.Product
+import com.example.domain.model.SearchRequest
 import com.example.domain.repository.ProductRepository
 import io.ktor.server.application.*
 import io.ktor.server.auth.*
 import io.ktor.server.plugins.*
+import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
@@ -14,19 +16,20 @@ fun Application.productRoutes(repo: ProductRepository) = routing {
     getByCategory(repo)
     getBestseller(repo)
     getFavoriteByUserId(repo)
+    search(repo)
 }
 
 fun Route.getAll(repo: ProductRepository) = get("/products") {
     repo.all().getOrThrow().let { productList ->
-            call.respond(productList)
-        }
+        call.respond(productList)
+    }
 }
 
 fun Route.getById(repo: ProductRepository) = get("/products/{id?}") {
     call.parameters["id"]?.toInt()?.let { id ->
         repo.findById(id).getOrThrow().let { product ->
-                call.respond(product)
-            }
+            call.respond(product)
+        }
     } ?: throw MissingRequestParameterException("ID")
 }
 
@@ -40,16 +43,24 @@ fun Route.getByCategory(repo: ProductRepository) = get("/products/category/{cate
 
 fun Route.getBestseller(repo: ProductRepository) = get("/products/bestseller") {
     repo.findBestsellers().getOrThrow().let { productList ->
-            call.respond(productList)
-        }
+        call.respond(productList)
+    }
 }
 
 fun Route.getFavoriteByUserId(repo: ProductRepository) = authenticate {
     get("/products/favorite/{userId?}") {
         call.parameters["userId"]?.toInt()?.let { userId ->
             repo.findFavoriteProduct(userId).getOrThrow().let { productList ->
-                    call.respond(productList)
-                }
+                call.respond(productList)
+            }
         } ?: throw MissingRequestParameterException("user ID")
+    }
+}
+
+fun Route.search(repo: ProductRepository) = post("/products/search") {
+    call.receive<SearchRequest>().let { request ->
+        repo.search(request).getOrThrow().let { productList ->
+            call.respond(productList)
+        }
     }
 }

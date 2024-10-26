0d74543392ada4f82cc98992d4be0b18e41bcdf2
abdozcan <abdozcann@gmail.com>
2024-10-26 16:24:57 +0300

    added `DATE` enum constant to SortOption

--- src/main/kotlin/com/example/domain/utils/SortOption.kt
+++ src/main/kotlin/com/example/domain/utils/SortOption.kt
@@ -3,5 +3,6 @@ package com.example.domain.utils
 enum class SortOption(val value: String) {
     PRICE_ASC("price_asc"),
     PRICE_DESC("price_desc"),
-    POPULARITY("popularity")
+    POPULARITY("popularity"),
+    DATE("date")
 }

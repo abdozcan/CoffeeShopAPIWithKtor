aa8d64352f9288bea944ee801da39331cfc7b4b3
abdozcan <abdozcann@gmail.com>
2024-10-26 14:33:03 +0300

    added `limit` and `page` parameters to get routes in reviewRoutes

--- src/main/kotlin/com/example/routes/ReviewRoutes.kt
+++ src/main/kotlin/com/example/routes/ReviewRoutes.kt
@@ -2,6 +2,7 @@ package com.example.routes
 
 import com.example.domain.model.Review
 import com.example.domain.repository.ReviewRepository
+import com.example.routes.utils.by
 import io.ktor.server.application.*
 import io.ktor.server.plugins.*
 import io.ktor.server.request.*
@@ -9,30 +10,32 @@ import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
 fun Application.reviewRoutes(reviewRepo: ReviewRepository) = routing {
-    getAllByProductId(reviewRepo)
-    getAllByUserId(reviewRepo)
-    add(reviewRepo)
-    edit(reviewRepo)
-    delete(reviewRepo)
+    route("/review") {
+        getAllByProductId(reviewRepo)
+        getAllByUserId(reviewRepo)
+        add(reviewRepo)
+        edit(reviewRepo)
+        delete(reviewRepo)
+    }
 }
 
-fun Route.getAllByProductId(repo: ReviewRepository) = get("review/product/{productId?}") {
-    call.parameters["productId"]?.toInt()?.let { id ->
-        repo.findAllByProductId(id).getOrThrow().let { reviewList ->
+fun Route.getAllByProductId(repo: ReviewRepository) = route("/product") {
+    by { id, limit, offset ->
+        repo.findAllByProductId(id, limit, offset).getOrThrow().let { reviewList ->
             call.respond(reviewList)
         }
-    } ?: throw MissingRequestParameterException("product ID")
+    }
 }
 
-fun Route.getAllByUserId(repo: ReviewRepository) = get("review/user/{userId?}") {
-    call.parameters["userId"]?.toInt()?.let { id ->
-        repo.findAllByUserId(id).getOrThrow().let { reviewList ->
+fun Route.getAllByUserId(repo: ReviewRepository) = route("/user") {
+    by { id, limit, offset ->
+        repo.findAllByUserId(id, limit, offset).getOrThrow().let { reviewList ->
             call.respond(reviewList)
         }
-    } ?: throw MissingRequestParameterException("user ID")
+    }
 }
 
-fun Route.add(repo: ReviewRepository) = post("review/add") {
+fun Route.add(repo: ReviewRepository) = post("/add") {
     call.receive<Review>().let { review ->
         repo.add(review).getOrThrow().let {
             call.respond("Review added successfully.")
@@ -40,7 +43,7 @@ fun Route.add(repo: ReviewRepository) = post("review/add") {
     }
 }
 
-fun Route.edit(repo: ReviewRepository) = put("review/edit") {
+fun Route.edit(repo: ReviewRepository) = put("/edit") {
     call.receive<Review>().let { review ->
         repo.edit(review).getOrThrow().let {
             call.respond("Review edited successfully.")
@@ -48,7 +51,7 @@ fun Route.edit(repo: ReviewRepository) = put("review/edit") {
     }
 }
 
-fun Route.delete(repo: ReviewRepository) = delete("review/delete/{id?}") {
+fun Route.delete(repo: ReviewRepository) = delete("/delete/{id?}") {
     call.parameters["id"]?.toInt()?.let { id ->
         repo.delete(id).getOrThrow()
         call.respond("Review deleted successfully.")

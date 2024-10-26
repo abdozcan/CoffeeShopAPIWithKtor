1ec973cd24d4e67e233d8b9a2b4252a5945f94be
abdozcan <abdozcann@gmail.com>
2024-10-26 18:28:37 +0300

    refactored `by` extension functions naming as `getBy` to accept SortOption`s subclasses and added `sortOption` parameter to `block` parameter

--- src/main/kotlin/com/example/routes/utils/Extension.kt
+++ src/main/kotlin/com/example/routes/utils/Extension.kt
@@ -1,23 +1,37 @@
 package com.example.routes.utils
 
+import com.example.domain.utils.ProductSortOption
+import com.example.domain.utils.ReviewSortOption
+import com.example.domain.utils.SortOption
 import io.ktor.server.plugins.*
 import io.ktor.server.routing.*
 
-fun Route.by(block: suspend RoutingContext.(id: Int, limit: Int, offset: Long) -> Unit) =
-    route("/{id?}") {
-        by { limit, offset ->
-            call.parameters["id"]?.toInt()?.let { id ->
-                block(id, limit, offset)
-            } ?: throw MissingRequestParameterException("ID")
-        }
+inline fun <reified T : SortOption> Route.getBy(
+    crossinline block: suspend RoutingContext.(id: Int, limit: Int, offset: Long, sortOption: T) -> Unit
+) = route("/{id?}") {
+    getBy<T> { limit, offset, sortOption ->
+        call.parameters["id"]?.toInt()?.let { id ->
+            block(id, limit, offset, sortOption)
+        } ?: throw MissingRequestParameterException("ID")
     }
+}
 
-fun Route.by(block: suspend RoutingContext.(limit: Int, offset: Long) -> Unit) =
-    get("/limit/{limit?}/page/{page?}") {
-        call.parameters["limit"]?.toInt()?.let { limit ->
-            call.parameters["page"]?.toLong()?.let { page ->
+inline fun <reified T : SortOption> Route.getBy(
+    crossinline block: suspend RoutingContext.(limit: Int, offset: Long, sortOption: T) -> Unit
+) = get("/limit/{limit?}/page/{page?}") {
+    call.parameters["limit"]?.toInt()?.let { limit ->
+        call.parameters["page"]?.toLong()?.let { page ->
+            call.request.queryParameters["sort"].let { sort ->
                 val offset = (page - 1) * limit
-                block(limit, offset)
-            } ?: throw MissingRequestParameterException("page")
-        } ?: throw MissingRequestParameterException("limit")
-    }
+
+                val sortOption = when (T::class) {
+                    ProductSortOption::class -> ProductSortOption.valueOf(sort ?: ProductSortOption.DATE_DESC.name) as T
+                    ReviewSortOption::class -> ReviewSortOption.valueOf(sort ?: ReviewSortOption.DATE_DESC.name) as T
+                    else -> throw IllegalArgumentException("Unsupported SortOption type")
+                }
+
+                block(limit, offset, sortOption)
+            }
+        } ?: throw MissingRequestParameterException("page")
+    } ?: throw MissingRequestParameterException("limit")
+}

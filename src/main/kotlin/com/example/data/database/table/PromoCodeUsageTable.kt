c45301621582935388e063b751d171aa1d64bfcd
abdozcan <abdozcann@gmail.com>
2024-12-03 16:38:56 +0300

    created `PromoCodeUsageTable`

---
+++ src/main/kotlin/com/example/data/database/table/PromoCodeUsageTable.kt
@@ -0,0 +1,9 @@
+package com.example.data.database.table
+
+import org.jetbrains.exposed.dao.id.IntIdTable
+
+object PromoCodeUsageTable : IntIdTable("promo_code_usages") {
+    val userId = reference(name = "user_id", foreign = UserTable)
+    val promoCodeId = reference(name = "promo_code_id", foreign = PromoCodeTable)
+    val used = bool(name = "used")
+}
